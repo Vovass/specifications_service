9dae19844b86f59b9f198dc62655921858a23698
Uladzimir Dzemiankou <dzemiankou.u@profitero.com>
2021-11-21 22:34:16 +0300

    made general page for fields and variation added design

--- app/javascript/channels/field_channel.js
+++ app/javascript/channels/field_channel.js
@@ -1,12 +1,13 @@
 $(document).ready(function(){
-    $('#sel').change(function(){
+    $('.sel_desc').change(function(){
+        var field_number = $(this).attr('class').match(/result_hide_field_\d+/)[0];
         var optionSelected = $("option:selected", this);
         var number = parseInt(optionSelected.attr('class').match(/\d+/)[0]);
         $(".hide").each(function (index) {
             if (index == number) {
-                $(`.hide_result_${index}`).show();
+                $(`.hide_result_${index}.${field_number}`).show();
             } else {
-                $(`.hide_result_${index}`).hide();
+                $(`.hide_result_${index}.${field_number}`).hide();
             }
         });
     });
