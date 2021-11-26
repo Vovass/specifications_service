684d3875ec3903ed36f2da670de45a6ea3ea8cd0
Uladzimir Dzemiankou <dzemiankou.u@profitero.com>
2021-11-26 10:41:02 +0300

    webpack ready

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,8 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "bootstrap"
+import "trix/dist/trix"
 
 // import "bootstrap"
 // import "../stylesheets/application.scss"
@@ -22,3 +24,4 @@ ActiveStorage.start()
 require("trix")
 require("@rails/actiontext")
 require('jquery')
+// require('bootstrap-select')
